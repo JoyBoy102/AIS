96c236a089a2132edc37e1e982457b18ed62cd6a
Ilya <nynate@mail.ru>
2025-12-10 22:44:32 +0500

    fix manual module

---
+++ AIS/Converters/ObjectToVisibility.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace AIS.Converters
+{
+    public class ObjectToVisibility: IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool boolValue = value != null;
+            bool isInverse = parameter != null && parameter.ToString() == "inverse";
+
+            if (isInverse)
+            {
+                boolValue = !boolValue;
+            }
+
+            return boolValue ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
